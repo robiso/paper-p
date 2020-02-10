5c356d753be6a6a6a776d957673b64cb58d9c539
Robert Isoski <r@robiso.com>
2020-02-10 20:25:58 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -45,8 +45,8 @@ if(isset($_COOKIE['stylesheet'])) {
         <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
     	
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link id="stylesheet" rel="stylesheet" href="<?= $Wcms->asset($css) ?>">
         <?= $Wcms->css() ?>
+	<link id="stylesheet" rel="stylesheet" href="<?= $Wcms->asset($css) ?>">
 
     </head>
     <body>
